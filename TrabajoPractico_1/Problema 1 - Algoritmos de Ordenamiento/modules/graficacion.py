1330ee84de706345daf8d7d33f655d2abadbc22f
santivela5 <santyvl@outlook.com>
2025-04-12 15:44:44 -0300

    agrego los modulos de tiempos y graficacion

---
+++ TrabajoPractico_1/Problema 1 - Algoritmos de Ordenamiento/modules/graficacion.py
@@ -0,0 +1,31 @@
+from matplotlib import pyplot as plt
+from tiempos import medir_tiempos
+
+def graficar_tiempos(lista_metodos_ord):
+    #tamanos = [1, 10, 100, 200, 300, 400, 500, 600, 700, 800, 900, 1000]
+    tamanos = [n for n in range(0, 1001, 5)]
+
+    # figsize es el tamaño de la figura en pulgadas (width, height)
+    plt.figure(figsize=(10, 6))
+
+    for metodo_ord in lista_metodos_ord:
+        
+        tiempos = medir_tiempos(metodo_ord, tamanos)
+
+        # plot es para graficar los tiempos de ordenamiento
+        # plot es el método de matplotlib para graficar
+        # marker='o' es para poner un punto en cada coordenada
+        plt.plot(tamanos, tiempos, marker='o', label=metodo_ord.__name__)
+
+    plt.xlabel('Tamaño de la lista')
+    plt.ylabel('Tiempo (segundos)')
+    plt.title('Comparación de tiempos de ordenamiento')
+    plt.legend() # para mostrar el nombre del método de ordenamiento. Es el "label" del metodo plot
+    plt.grid() # cuadriculado
+    plt.show()
+
+if __name__ == '__main__':
+   from burbuja import burbuja
+   from quicksort import ordenamientoRapido
+   #from modules.radixsort import radix_sort
+   graficar_tiempos([burbuja, ordenamientoRapido])
---
+++ TrabajoPractico_1/Problema 1 - Algoritmos de Ordenamiento/modules/tiempos.py
@@ -0,0 +1,25 @@
+from random import randint
+import time
+
+def medir_tiempos(metodo_ord, tamanos):
+    tiempos_ord_selecc = []
+
+    for n in tamanos:
+        # datos = []
+        # for _ in range(n):
+        #     datos.append(randint(1, 10000))
+        datos = [randint(1, 10000) for _ in range(n)]
+
+        inicio = time.perf_counter()
+        metodo_ord(datos)
+        fin = time.perf_counter()
+        tiempos_ord_selecc.append(fin - inicio)
+        
+        print(f"Tiempo de ordenamiento por seleccion para n={n}: {fin - inicio:.6f} segundos")
+    
+    return tiempos_ord_selecc
+
+if __name__ == '__main__':
+    tamanos = [1, 10, 100, 200, 500, 700, 1000]
+    medir_tiempos(tamanos)
+    
