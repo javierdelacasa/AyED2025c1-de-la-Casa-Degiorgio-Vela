fd30ba7d2174542f3968494b5b7a61eeb1c3781f
Edu-deg <Degiorgioeduardo@gmail.com>
2025-04-27 18:54:03 -0300

    Graficas de N vs tiempos de ejecucion (problema 2)

---
+++ TrabajoPractico_1/Problema 2 - Lista doblemente enlazada/modules/graficacion.py
@@ -0,0 +1,74 @@
+from matplotlib import pyplot as plt
+from tiempos import medir_tiempos_len
+from tiempos import medir_tiempos_copia
+from tiempos import medir_tiempos_invertir
+
+
+def graficar_tiempos_len():
+    #tamanos = [1, 10, 100, 200, 300, 400, 500, 600, 700, 800, 900, 1000]
+    tamanos = [n for n in range(1, 1001, 20)]
+    # figsize es el tamaño de la figura en pulgadas (width, height)
+    plt.figure(figsize=(10, 6))
+
+        
+    tiempos = medir_tiempos_len(tamanos)
+
+    # plot es para graficar los tiempos de ordenamiento
+    # plot es el método de matplotlib para graficar
+    # marker='o' es para poner un punto en cada coordenada
+    plt.plot(tamanos, tiempos, marker='o', label="funcion len")
+
+    plt.xlabel('Tamaño de la lista')
+    plt.ylabel('Tiempo (segundos)')
+    plt.title('Tiempos de la funcion len')
+    plt.legend() # para mostrar el nombre del método de ordenamiento. Es el "label" del metodo plot
+    plt.grid() # cuadriculado
+    plt.show()
+
+def graficar_tiempos_copia():
+    #tamanos = [1, 10, 100, 200, 300, 400, 500, 600, 700, 800, 900, 1000]
+    tamanos = [n for n in range(1, 1001, 20)]
+    # figsize es el tamaño de la figura en pulgadas (width, height)
+    plt.figure(figsize=(10, 6))
+
+        
+    tiempos = medir_tiempos_copia(tamanos)
+
+    # plot es para graficar los tiempos de ordenamiento
+    # plot es el método de matplotlib para graficar
+    # marker='o' es para poner un punto en cada coordenada
+    plt.plot(tamanos, tiempos, marker='o', label="funcion copia")
+
+    plt.xlabel('Tamaño de la lista')
+    plt.ylabel('Tiempo (segundos)')
+    plt.title('Tiempos de la funcion copia')
+    plt.legend() # para mostrar el nombre del método de ordenamiento. Es el "label" del metodo plot
+    plt.grid() # cuadriculado
+    plt.show()
+
+def graficar_tiempos_invertir():
+    #tamanos = [1, 10, 100, 200, 300, 400, 500, 600, 700, 800, 900, 1000]
+    tamanos = [n for n in range(1, 1001, 20)]
+    # figsize es el tamaño de la figura en pulgadas (width, height)
+    plt.figure(figsize=(10, 6))
+
+        
+    tiempos = medir_tiempos_invertir(tamanos)
+
+    # plot es para graficar los tiempos de ordenamiento
+    # plot es el método de matplotlib para graficar
+    # marker='o' es para poner un punto en cada coordenada
+    plt.plot(tamanos, tiempos, marker='o', label="funcion invertir")
+
+    plt.xlabel('Tamaño de la lista')
+    plt.ylabel('Tiempo (segundos)')
+    plt.title('Tiempos de la funcion invertir')
+    plt.legend() # para mostrar el nombre del método de ordenamiento. Es el "label" del metodo plot
+    plt.grid() # cuadriculado
+    plt.show()
+
+if __name__ == '__main__':
+   graficar_tiempos_len()
+   graficar_tiempos_copia()
+   graficar_tiempos_invertir()
+   
---
+++ TrabajoPractico_1/Problema 2 - Lista doblemente enlazada/modules/tiempos.py
@@ -0,0 +1,65 @@
+from random import randint
+import time
+from ListaDobleEnlazada import ListaDobleEnlazada
+
+def medir_tiempos_len(tamanos):
+    
+    tiempos_len= []
+
+    for n in tamanos:
+        lista = ListaDobleEnlazada()
+        for i in range(n):
+            lista.agregar_al_final(randint(1, 1000))
+            
+        inicio = time.perf_counter()
+        lista.__len__
+        fin = time.perf_counter()
+        tiempos_len.append(fin - inicio)
+        print(f"Tiempo de funcion len para n={n}: {fin - inicio:.6f} segundos")
+    
+    return tiempos_len  
+
+def medir_tiempos_copia(tamanos):
+    
+    tiempos_copia = []
+    
+    for n in tamanos:
+        lista = ListaDobleEnlazada()
+        for i in range(n):
+            lista.agregar_al_final(randint(1, 1000))
+        inicio = time.perf_counter()
+        lista.copiar
+        fin = time.perf_counter()
+        tiempos_copia.append(fin - inicio)
+        print(f"Tiempo de funcion copia para n={n}: {fin - inicio:.6f} segundos")
+    
+    return tiempos_copia
+
+def medir_tiempos_invertir(tamanos):
+    
+    tiempos_invertir = []
+    
+    for n in tamanos:
+        lista = ListaDobleEnlazada()
+        for i in range(n):
+            lista.agregar_al_final(randint(1, 1000))
+        inicio = time.perf_counter()
+        lista.invertir
+        fin = time.perf_counter()
+        tiempos_invertir.append(fin - inicio)
+        print(f"Tiempo de funcion invertir para n={n}: {fin - inicio:.6f} segundos")
+    
+    return tiempos_invertir
+        
+        
+
+    
+
+if __name__ == '__main__':
+    tamanos = [1, 10, 100, 200, 500, 700, 1000]
+    
+    medir_tiempos_len(tamanos)
+    
+    medir_tiempos_copia(tamanos)
+    
+    medir_tiempos_invertir(tamanos)
