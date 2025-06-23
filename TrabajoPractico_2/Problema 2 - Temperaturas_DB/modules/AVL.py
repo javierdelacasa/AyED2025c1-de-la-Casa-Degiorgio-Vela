71f67c5915b66c3600a1d386616edb47392e937c
santivela5 <santyvl@outlook.com>
2025-06-23 10:30:47 -0300

    agrego comentarios a modulos del problema 2

--- TrabajoPractico_2/Problema 2 - Temperaturas_DB/modules/AVL.py
+++ TrabajoPractico_2/Problema 2 - Temperaturas_DB/modules/AVL.py
@@ -25,12 +25,13 @@ class Nodo:
     def esRaiz(self):
         return self.padre is None
     
-    def _obtenermaximo(self):
+    def _obtenermaximo(self): #funcion auxiliar de obtenermaximo
         cargautil_max = self.cargautil
         clave_max = self.clave
+        #Actua de manera recursiva
         if self.tieneHijoIzquierdo():
             hijo_clave_max, hijo_cargautil_max = self.hijoIzquierdo._obtenermaximo()
-            if hijo_cargautil_max > cargautil_max:
+            if hijo_cargautil_max > cargautil_max: #se guarda la carga util mayor
                 cargautil_max = hijo_cargautil_max
                 clave_max = hijo_clave_max
         if self.tieneHijoDerecho():
@@ -38,14 +39,15 @@ class Nodo:
             if hijo_cargautil_max > cargautil_max:
                 cargautil_max = hijo_cargautil_max
                 clave_max = hijo_clave_max
-        return clave_max, cargautil_max
+        return clave_max, cargautil_max #clave asociada a la carga util maxima y carga util maxima
 
-    def _obtenerminimo(self):
+    def _obtenerminimo(self): #funcion auxiliar de obtenerminimo
         cargautil_min = self.cargautil
         clave_min = self.clave
+        #Actua de manera recursiva
         if self.tieneHijoIzquierdo():
             hijo_clave_min, hijo_cargautil_min = self.hijoIzquierdo._obtenerminimo()
-            if hijo_cargautil_min < cargautil_min:
+            if hijo_cargautil_min < cargautil_min:# se guarda la carga util menor
                 cargautil_min = hijo_cargautil_min
                 clave_min = hijo_clave_min
         if self.tieneHijoDerecho():
@@ -53,22 +55,18 @@ class Nodo:
             if hijo_cargautil_min < cargautil_min:
                 cargautil_min = hijo_cargautil_min
                 clave_min = hijo_clave_min
-        return clave_min, cargautil_min
+        return clave_min, cargautil_min #clave asociada a la carga util minima y carga util minima
     
     def _obtenerminimo_rango(self, inicio, fin):
         clave_min, cargautil_min = None, float('inf')
-
-        # Verificar si el nodo actual está dentro del rango
+        # Verificar si el nodo actual está dentro del rango 
         if inicio <= self.clave <= fin:
             clave_min, cargautil_min = self.clave, self.cargautil
-
-        # Recorrer el hijo izquierdo si existe
+        # Actua recursivamente
         if self.tieneHijoIzquierdo():
             hijo_clave_min, hijo_cargautil_min = self.hijoIzquierdo._obtenerminimo_rango(inicio, fin)
             if hijo_cargautil_min < cargautil_min:
                 clave_min, cargautil_min = hijo_clave_min, hijo_cargautil_min
-
-        # Recorrer el hijo derecho si existe
         if self.tieneHijoDerecho():
             hijo_clave_min, hijo_cargautil_min = self.hijoDerecho._obtenerminimo_rango(inicio, fin)
             if hijo_cargautil_min < cargautil_min:
@@ -78,18 +76,14 @@ class Nodo:
 
     def _obtenermaximo_rango(self, inicio, fin):
         clave_max, cargautil_max = None, float('-inf')
-
-        # Verificar si el nodo actual está dentro del rango
+        # Verifica si el nodo actual está dentro del rango
         if inicio <= self.clave <= fin:
             clave_max, cargautil_max = self.clave, self.cargautil
-
-        # Recorrer el hijo izquierdo si existe
+        # Actua recursivamente
         if self.tieneHijoIzquierdo():
             hijo_clave_max, hijo_cargautil_max = self.hijoIzquierdo._obtenermaximo_rango(inicio, fin)
             if hijo_cargautil_max > cargautil_max:
                 clave_max, cargautil_max = hijo_clave_max, hijo_cargautil_max
-
-        # Recorrer el hijo derecho si existe
         if self.tieneHijoDerecho():
             hijo_clave_max, hijo_cargautil_max = self.hijoDerecho._obtenermaximo_rango(inicio, fin)
             if hijo_cargautil_max > cargautil_max:
@@ -97,31 +91,33 @@ class Nodo:
 
         return clave_max, cargautil_max
     
-    def preorden(self):
-        print(self.clave)
+    def preorden(self): #funcion auxiliar de preorden_visualizacion
+        print(self.clave) # imprime la clave del nodo
         if self.hijoIzquierdo:
             self.hijoIzquierdo.preorden()
         if self.hijoDerecho:
             self.hijoDerecho.preorden()
     
-    def preorden_rango(self, inicio, fin):
+    def preorden_rango(self, inicio, fin): #funcion auxiliar de preorden_rango_visualizacion
+        # Verifica si la clave del nodo actual está dentro del rango
         if inicio <= self.clave <= fin:
             print(self.clave)
         if self.tieneHijoIzquierdo() and self.clave > inicio:
             self.hijoIzquierdo.preorden_rango(inicio, fin)
         if self.tieneHijoDerecho() and self.clave < fin:
             self.hijoDerecho.preorden_rango(inicio, fin)
-
-    def preorden_obtencion(self):
-        print(self.cargautil)
+ 
+    def preorden_obtencion(self):# funcion auxiliar de preorden_obtencion
+        print(self.cargautil) # imprime la carga util del nodo
         if self.hijoIzquierdo:
             self.hijoIzquierdo.preorden_obtencion()
         if self.hijoDerecho:
             self.hijoDerecho.preorden_obtencion()
     
-    def preorden_rango_obtencion(self, inicio, fin):
+    def preorden_rango_obtencion(self, inicio, fin): # funcion auxiliar de preorden_rango_obtencion
+        # Verifica si está dentro del rango
         if inicio <= self.clave <= fin:
-            print(self.clave,self.cargautil)
+            print(self.clave,self.cargautil) # imprime la clave y la carga util del nodo
         if self.tieneHijoIzquierdo() and self.clave > inicio:
             self.hijoIzquierdo.preorden_rango_obtencion(inicio, fin)
         if self.tieneHijoDerecho() and self.clave < fin:
@@ -145,13 +141,13 @@ class Nodo:
 
     def encontrarSucesor(self):
         suc = None
-        if self.tieneHijoDerecho():
+        if self.tieneHijoDerecho(): # si tiene hijo derecho, el sucesor es el mínimo del subárbol derecho
             suc = self.hijoDerecho.encontrarMin()
         else:
-            if self.padre:
-                if self.esHijoIzquierdo():
+            if self.padre: 
+                if self.esHijoIzquierdo(): # si es hijo izquierdo, el sucesor es el padre
                     suc = self.padre
-                else:
+                else: # si es hijo derecho, se busca el sucesor en el padre
                     self.padre.hijoDerecho = None
                     suc = self.padre.encontrarSucesor()
                     self.padre.hijoDerecho = self
@@ -159,17 +155,17 @@ class Nodo:
 
     def encontrarMin(self):
         actual = self
-        while actual.tieneHijoIzquierdo():
+        while actual.tieneHijoIzquierdo(): #el minimo se encuentra en el extremo izquierdo
             actual = actual.hijoIzquierdo
         return actual
 
     def empalmar(self):
-        if self.esHoja():
+        if self.esHoja(): # si es hoja, simplemente se elimina
             if self.esHijoIzquierdo():
                 self.padre.hijoIzquierdo = None
             else:
                 self.padre.hijoDerecho = None
-        elif self.tieneAlgunHijo():
+        elif self.tieneAlgunHijo(): # se conectan las referencias entre el padre y algun hijo
             if self.tieneHijoIzquierdo():
                 if self.esHijoIzquierdo():
                     self.padre.hijoIzquierdo = self.hijoIzquierdo
@@ -183,12 +179,12 @@ class Nodo:
                     self.padre.hijoDerecho = self.hijoDerecho
                 self.hijoDerecho.padre = self.padre
 
-    def __iter__(self):
+    def __iter__(self): # permite iterar sobre el nodo y sus hijos con un inorden
         if self.hijoIzquierdo:
-            yield from self.hijoIzquierdo
-        yield self.clave, self.cargautil
+            yield from self.hijoIzquierdo # se ejecuta sobre el hijo izquierdo
+        yield self.clave, self.cargautil # devuelve la clave y la carga util del nodo
         if self.hijoDerecho:
-            yield from self.hijoDerecho
+            yield from self.hijoDerecho # se ejecuta sobre el hijo derecho
 
 class Arbol:
     def __init__(self):
@@ -202,22 +198,22 @@ class Arbol:
         return self.tamano
 
     def __iter__(self):
-        return self.raiz.__iter__()
+        return self.raiz.__iter__() ######
     
     def agregar(self,clave,valor):
         if self.raiz:
             self._agregar(clave,valor,self.raiz)
         else:
-            self.raiz = Nodo(clave,valor)
+            self.raiz = Nodo(clave,valor) #si el arbol está vacío, se crea un nuevo nodo raíz
         self.tamano = self.tamano + 1
 
-    def _agregar(self,clave,valor,nodoActual):
+    def _agregar(self,clave,valor,nodoActual): #funcion auxiliar de agregar
         if clave < nodoActual.clave:
-                if nodoActual.tieneHijoIzquierdo():
-                        self._agregar(clave,valor,nodoActual.hijoIzquierdo)
-                else:
-                        nodoActual.hijoIzquierdo = Nodo(clave,valor,padre=nodoActual)
-                        self.actualizarEquilibrio(nodoActual.hijoIzquierdo)
+            if nodoActual.tieneHijoIzquierdo():
+                    self._agregar(clave,valor,nodoActual.hijoIzquierdo) #se ejecuta recursivamente
+            else:
+                    nodoActual.hijoIzquierdo = Nodo(clave,valor,padre=nodoActual) #se crea el nodo en su lugar
+                    self.actualizarEquilibrio(nodoActual.hijoIzquierdo)
         else:
             if nodoActual.tieneHijoDerecho():
                     self._agregar(clave,valor,nodoActual.hijoDerecho)
@@ -226,10 +222,10 @@ class Arbol:
                     self.actualizarEquilibrio(nodoActual.hijoDerecho)
 
     def actualizarEquilibrio(self,nodo):
-        if nodo.factorEquilibrio > 1 or nodo.factorEquilibrio < -1:
+        if nodo.factorEquilibrio > 1 or nodo.factorEquilibrio < -1: # si esta desequilibrado se reequilibra
             self.reequilibrar(nodo)
             return
-        if nodo.padre != None:
+        if nodo.padre != None:  # si no es raiz se actualiza el fe del padre
             if nodo.esHijoIzquierdo():
                     nodo.padre.factorEquilibrio += 1
             elif nodo.esHijoDerecho():
@@ -248,7 +244,7 @@ class Arbol:
             self.raiz = nuevaRaiz
         else:
             if rotRaiz.esHijoIzquierdo():
-                    rotRaiz.padre.hijoIzquierdo = nuevaRaiz
+                rotRaiz.padre.hijoIzquierdo = nuevaRaiz
             else:
                 rotRaiz.padre.hijoDerecho = nuevaRaiz
         nuevaRaiz.hijoIzquierdo = rotRaiz
@@ -274,7 +270,7 @@ class Arbol:
         rotRaiz.factorEquilibrio = rotRaiz.factorEquilibrio - 1 - max(nuevaRaiz.factorEquilibrio, 0)
         nuevaRaiz.factorEquilibrio = nuevaRaiz.factorEquilibrio - 1 + min(rotRaiz.factorEquilibrio, 0)
 
-    def reequilibrar(self,nodo):
+    def reequilibrar(self,nodo): # realiza las rotaciones que correspondan
         if nodo.factorEquilibrio < 0:
                 if nodo.hijoDerecho.factorEquilibrio > 0:
                     self.rotarDerecha(nodo.hijoDerecho)
@@ -291,108 +287,112 @@ class Arbol:
     def obtener(self, clave):
         actual = self.raiz
         while True:
-            if actual.clave == clave:
+            if actual.clave == clave: # comprueba en cada nodo si la clave es la buscada
                 return actual.cargautil
+            # cambia el actual y repite el ciclo
             elif actual.clave < clave and actual.tieneHijoDerecho():
                     actual = actual.hijoDerecho
             elif actual.clave > clave and actual.tieneHijoIzquierdo():
                     actual = actual.hijoIzquierdo
             else:
-                return None
+                return None 
             
-    def __getitem__(self,clave):
+    def __getitem__(self,clave): 
         return self.obtener(clave)
 
     def obtenermaximo(self):
         if self.raiz is not None:
-            return self.raiz._obtenermaximo()
+            return self.raiz._obtenermaximo() #llama a la funcion auxiliar sobre la raiz
         return None
 
     def obtenerminimo(self):
         if self.raiz is not None:
-            return self.raiz._obtenerminimo()
+            return self.raiz._obtenerminimo() #llama a la funcion auxiliar sobre la raiz
         return None
     
     def obtenermaximo_rango(self,inicio, fin):
         if self.raiz is not None:
-            return self.raiz._obtenermaximo_rango(inicio, fin)
+            return self.raiz._obtenermaximo_rango(inicio, fin) #llama a la funcion auxiliar sobre la raiz
         return None
 
     def obtenerminimo_rango(self,inicio, fin):
         if self.raiz is not None:
-            return self.raiz._obtenerminimo_rango(inicio, fin)
+            return self.raiz._obtenerminimo_rango(inicio, fin) #llama a la funcion auxiliar sobre la raiz
         return None
 
     def preorden_visualizacion(self):
         if self.raiz is not None:
-            self.raiz.preorden()
+            self.raiz.preorden() #llama a la funcion auxiliar sobre la raiz
         else:
             print("El árbol está vacío.")
 
     def preorden_rango_visualizacion(self, inicio, fin):
         if self.raiz is not None:
-            self.raiz.preorden_rango(inicio, fin)
+            self.raiz.preorden_rango(inicio, fin) #llama a la funcion auxiliar sobre la raiz
         else:
             print("El árbol está vacío.")
 
     def preorden_obtencion(self):
         if self.raiz is not None:
-            self.raiz.preorden_obtencion()
+            self.raiz.preorden_obtencion() #llama a la funcion auxiliar sobre la raiz
         else:
             print("El árbol está vacío.")
 
     def preorden_rango_obtencion(self, inicio, fin):
         if self.raiz is not None:
-            self.raiz.preorden_rango_obtencion(inicio, fin)
+            self.raiz.preorden_rango_obtencion(inicio, fin) #llama a la funcion auxiliar sobre la raiz
         else:
             print("El árbol está vacío.")
 
     def remover (self,nodoActual):
-        if nodoActual.esHoja():
+        if nodoActual.esHoja(): #si es hoja, simplemente se elimina
             if nodoActual == nodoActual.padre.hijoIzquierdo:
                 nodoActual.padre.hijoIzquierdo = None
             else:
                 nodoActual.padre.hijoDerecho = None
         elif nodoActual.tieneAmbosHijos(): #interior
+            # se busca el sucesor, se empalma y se reemplaza la clave y la carga util
             suc = nodoActual.encontrarSucesor()
             suc.empalmar()
             nodoActual.clave = suc.clave
             nodoActual.cargautil = suc.cargautil
-        else: # este nodo tiene un (1) hijo
+        else: # (1) hijo
             if nodoActual.tieneHijoIzquierdo():
+                # se conecta el padre con el hijo izquierdo
                 if nodoActual.esHijoIzquierdo():
                     nodoActual.hijoIzquierdo.padre = nodoActual.padre
                     nodoActual.padre.hijoIzquierdo = nodoActual.hijoIzquierdo
                 elif nodoActual.esHijoDerecho():
                     nodoActual.hijoIzquierdo.padre = nodoActual.padre
                     nodoActual.padre.hijoDerecho = nodoActual.hijoIzquierdo
-                else:
+                else: # si es la raiz, se reemplaza el nodo actual por el hijo izquierdo
                     nodoActual.reemplazarDatoDeNodo(nodoActual.hijoIzquierdo.clave,
                                         nodoActual.hijoIzquierdo.cargautil,
                                         nodoActual.hijoIzquierdo.hijoIzquierdo,
                                         nodoActual.hijoIzquierdo.hijoDerecho)
                     nodoActual.padre = None 
-            else:
+            else: # si tiene hijo derecho, se conecta el padre con el hijo derecho
                 if nodoActual.esHijoIzquierdo():
                     nodoActual.hijoDerecho.padre = nodoActual.padre
                     nodoActual.padre.hijoIzquierdo = nodoActual.hijoDerecho
                 elif nodoActual.esHijoDerecho():
                     nodoActual.hijoDerecho.padre = nodoActual.padre
                     nodoActual.padre.hijoDerecho = nodoActual.hijoDerecho
-                else:
+                else: # si es la raiz, se reemplaza el nodo actual por el hijo derecho
                     nodoActual.reemplazarDatoDeNodo(nodoActual.hijoDerecho.clave,
                                         nodoActual.hijoDerecho.cargautil,
                                         nodoActual.hijoDerecho.hijoIzquierdo,
                                 nodoActual.hijoDerecho.hijoDerecho)    
+                    
     def eliminar(self,clave):
         if self.tamano > 1:
-            nodoAEliminar = self._obtener(clave, self.raiz)
+            nodoAEliminar = self._obtener(clave, self.raiz) # busca el nodo a eliminar
             if nodoAEliminar:
-                self.remover(nodoAEliminar)
+                self.remover(nodoAEliminar) # llama a la funcion auxiliar de remover
                 self.tamano = self.tamano-1
             else:
                 raise KeyError('Error, la clave no está en el árbol')
-        elif self.tamano == 1 and self.raiz.clave == clave:
+        elif self.tamano == 1 and self.raiz.clave == clave: # si es el único nodo, se elimina la raíz
             self.raiz = None
             self.tamano = self.tamano - 1
         else:
@@ -403,6 +403,7 @@ class Arbol:
         self.eliminar(clave)
 
     def _obtener(self, clave, nodoActual):
+        # función auxiliar de obtener, busca el nodo con la clave dada, recursivamente
         if nodoActual is None:
             return None
         if nodoActual.clave == clave:
@@ -416,7 +417,7 @@ class Arbol:
         
     def __iter__(self):
         if self.raiz is not None:
-            yield from self.raiz
+            yield from self.raiz #delega la iteración al nodo raíz
         
 
 
@@ -424,27 +425,3 @@ if __name__ == "__main__":
     arbol = Arbol()
     for clave in [46,34.5,25,12.23,30, 20, 10, 3 ,2, 1]:
         arbol.agregar(clave, clave * 2)
-    print("Tamaño del árbol:", arbol.tamanio())
-    print("Raíz del árbol:", arbol.raiz.clave)
-    #Deberia de imprimir el tamaño del árbol, la raíz, sus hijos y los factores de equilibrio. Esto es: 
-    print(arbol.raiz.hijoIzquierdo.clave)
-    print(arbol.raiz.hijoDerecho.clave)
-    print(arbol.raiz.hijoIzquierdo.factorEquilibrio)
-    print(arbol.obtener(30))
-    print(arbol.obtener(2))
-    print(arbol.obtenermaximo())
-    print(arbol.obtenerminimo())
-    print(arbol.obtenermaximo_rango(0, 40))
-    print(arbol.obtenerminimo_rango(9, 20))
-
-    
-
-
-    #Prueba rotaciones
-    arbol1 = Arbol()
-    for clave in [1,2,4,3,5,6,7]:
-        arbol1.agregar(clave, clave * 2)
-    arbol1.preorden_visualizacion()
-    arbol1.eliminar(4)
-    print("Después de eliminar 4:")
-    arbol1.preorden_visualizacion()
--- TrabajoPractico_2/Problema 2 - Temperaturas_DB/modules/Implementacion.py
+++ TrabajoPractico_2/Problema 2 - Temperaturas_DB/modules/Implementacion.py
@@ -3,51 +3,56 @@ import datetime
 
 class Temperatura_DB():
     def __init__(self):
-        self.arbol = Arbol()
+        self.arbol = Arbol() #crea un arbol AVL para almacenar las temperaturas
     
-    def convertir_fecha(self, fechastr):
+    def convertir_fecha(self, fechastr): #convierte una fecha en formato string a un objeto datetime
         if isinstance(fechastr, str):
             fecha = datetime.datetime.strptime(fechastr, "%d/%m/%Y")
             return fecha
 
     def guardar_temperatura(self, temperatura, fechastr=None):
-        if fechastr is None:
+        if fechastr is None: # Si no se proporciona una fecha, se usa la fecha actual
             fechastr = datetime.datetime.now().strftime("%d/%m/%Y")
-        elif isinstance(fechastr, datetime.datetime):
+        elif isinstance(fechastr, datetime.datetime): # Si se proporciona un objeto datetime, se convierte a string
             fechastr = fechastr.strftime("%d/%m/%Y")
-        fecha = self.convertir_fecha(fechastr)
-        self.arbol.agregar(fecha, temperatura)
+        fecha = self.convertir_fecha(fechastr) 
+        self.arbol.agregar(fecha, temperatura) #agrega al arbol con la fecha como clave
 
     def devolver_temperatura(self, fechastr):
-        fecha = self.convertir_fecha(fechastr)
+        fecha = self.convertir_fecha(fechastr) #convierte la fecha a datetime
         return self.arbol.obtener(fecha)
     
     def max_temp_rango(self, fecha_iniciostr, fecha_finstr):
         fecha_inicio = self.convertir_fecha(fecha_iniciostr)
         fecha_fin = self.convertir_fecha(fecha_finstr)
-        return self.arbol.obtenermaximo_rango(fecha_inicio, fecha_fin)[1]
+
+        return self.arbol.obtenermaximo_rango(fecha_inicio, fecha_fin)[1] #devuelve la carga util de la tupla obtenida
     
     def min_temp_rango(self, fecha_iniciostr, fecha_finstr):
         fecha_inicio = self.convertir_fecha(fecha_iniciostr)
         fecha_fin = self.convertir_fecha(fecha_finstr)
-        return self.arbol.obtenerminimo_rango(fecha_inicio, fecha_fin)[1]
+
+        return self.arbol.obtenerminimo_rango(fecha_inicio, fecha_fin)[1] #devuelve la carga util de la tupla obtenida
     
     def temp_extemos_rango(self, fecha_iniciostr, fecha_finstr):
         fecha_inicio = self.convertir_fecha(fecha_iniciostr)
         fecha_fin = self.convertir_fecha(fecha_finstr)
+
+        # utiliza las funciones max_temp_rango y min_temp_rango
         max_temp = self.arbol.obtenermaximo_rango(fecha_inicio, fecha_fin)[1]
         min_temp = self.arbol.obtenerminimo_rango(fecha_inicio, fecha_fin)[1]
         return (max_temp, min_temp)
     
     def devolver_todas_temperaturas(self):
-        for nodo in self.arbol:
+        for nodo in self.arbol: # Recorre el arbol AVL y devuelve todas las temperaturas
             print(f"Fecha: {nodo[0].strftime('%d/%m/%Y')}, Temperatura: {nodo[1]} ºC")
 
     def devolver_temperaturas(self, fecha_iniciostr, fecha_finstr):
         fecha_inicio = self.convertir_fecha(fecha_iniciostr)
         fecha_fin = self.convertir_fecha(fecha_finstr)
+        # Recorre el arbol 
         for nod in self.arbol:
-            if fecha_inicio <= nod[0] <= fecha_fin:
+            if fecha_inicio <= nod[0] <= fecha_fin: # Verifica si la fecha del nodo está dentro del rango
                 print(f"Fecha: {nod[0].strftime('%d/%m/%Y')}, Temperatura: {nod[1]} ºC")
 
     def cantidad_muestras(self):
